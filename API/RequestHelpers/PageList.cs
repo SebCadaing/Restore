96f3805291fecc19ffb54f40f7ceec72f3639189
Sebastian Cadaing <sebastiancadaing1@gmail.com>
2025-10-14 02:07:53 +0800

    End of Section 8

---
+++ API/RequestHelpers/PageList.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.RequestHelpers;
+
+public class PageList<T> : List<T> //Product
+{
+    public PageList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        Metadata = new PaginationMetadata
+        {
+            TotalCount = count,
+            PageSize = pageSize,
+            CurrentPage = pageNumber,
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+        };
+        AddRange(items);
+    }
+    public PaginationMetadata Metadata { get; set; }
+    
+    public static async Task<PageList<T>> ToPagedList(IQueryable<T> query, int pageNumber, int pageSize)
+    {
+        var count = await query.CountAsync();
+        var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        return new PageList<T>(items, count,pageNumber, pageSize);
+    }
+}
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,13 +1,39 @@
-import { useFetchProductsQuery } from "./catalogAPI";
+import { Grid, Typography } from "@mui/material";
+import { useFetchFiltersQuery, useFetchProductsQuery } from "./catalogAPI";
 import ProductList from "./ProductList";
+import Filters from "./Filters";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+import AppPagination from "../../app/shared/components/AppPagination";
+import { setPageNumber } from "./catalogSlice";
 
 export default function Catalog() {
-  const { data, isLoading } = useFetchProductsQuery();
-  if (isLoading || !data) return <div>loading...</div>;
+  const productParams = useAppSelector((state) => state.catalog);
+  const { data, isLoading } = useFetchProductsQuery(productParams);
+  const { data: filterrsData, isLoading: filtersLoading } = useFetchFiltersQuery();
+  const dispatch = useAppDispatch();
+  if (isLoading || !data || filtersLoading || !filterrsData) return <div>loading...</div>;
 
   return (
-    <>
-      <ProductList products={data} />
-    </>
+    <Grid container spacing={4}>
+      <Grid size={3}>
+        <Filters filtersData={filterrsData} />
+      </Grid>
+      <Grid size={9}>
+        {data.items && data.items.length > 0 ? (
+          <>
+            <ProductList products={data.items} />
+            <AppPagination
+              metadata={data.pagination}
+              onPageChange={(page: number) => {
+                dispatch(setPageNumber(page));
+                window.scrollTo({ top: 0, behavior: "smooth" });
+              }}
+            />
+          </>
+        ) : (
+          <Typography variant="h5">There are no result</Typography>
+        )}
+      </Grid>
+    </Grid>
   );
 }
