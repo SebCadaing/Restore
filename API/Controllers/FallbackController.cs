f112657d7011daa74f2fb3ddee768d9c973c5c6f
Sebastian Cadaing <sebastiancadaing1@gmail.com>
2025-10-23 19:50:04 +0800

    solution 3

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -8,6 +8,6 @@ public class FallbackController : Controller
 {
     public IActionResult Index()
     {
-        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/html");
     }
 }
